9cc79448ef0c6eb4b962ebd369827202795576ac
Stewart Hitchcock <Stewart.Hitchcock@gmail.com>
2020-09-30 10:52:09 +0100

    Feedback added

--- js/email-valid.js
+++ js/email-valid.js
@@ -1,60 +1,12 @@
-// function validateForm() {
-//     let x = document.forms["myForm"]["email"].value;
-//     if (x == "") {
-//       alert("Email cannot be blank");
-//       return false;
-//     } else{
-//         return true;
-//     }
-// }
-
-// function validate() {
-//     if( document.myForm.email.value == ""){ // Use null, work out how.
-//         window.alert("Please provide your email address.")
-//         document.myForm.email.focus();
-//         return false;
-//     } if(document.myForm.email.value.incudes("@")){
-
-
-//     } else {
-//         return true;
-//     }
-// }
-
-// function validateEmail() {
-//     let emailID = document.myForm.email.value;
-//     atpos = emailID.indexOf("@");
-//     dotpos = emailID.lastIndexOf(".");
-
-//     if (atpos < 1 || ( dotpos - atpos < 2 )) {
-//        alert("Please enter correct email ID")
-//        document.myForm.email.focus() ;
-//        return false;
-//     }
-//     return( true );
-//  }
-// submit.onClick = function() {
-
-//    let emailValue = email.value;
-
-//    console.log("true");
-//    if (emailValue.match("/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/")){
-//       console.log("true");
-//    }
-// }
-
-
 //Variables 
 
 // api
 const unsplash = "https://source.unsplash.com/random";
+let responseURL = "";
+
 const getImage = document.querySelector(".getImage");
 const imgCtn = document.querySelector(".img-ctn");
-let responseURL = "";
 const rightCtn = document.querySelector(".right-ctn");
-
-
-
 
 
 // Email validation
@@ -74,19 +26,51 @@ function warningFailAPIHide() {
    warningFailAPI.style.display = "none";
 }
 
+// function getNewImage() {
+//    try {
+//       fetch(unsplash)
+//          .then((response) => {
+//             responseURL = response.url;
+//             imgCtn.innerHTML = `<img src="${responseURL}">`;
+//          })
+//    } catch (e) {
+//       console.log(e);
+//       warningFailAPI.style.display = "block";
+//       setTimeout(warningfailAPIHide, 5000);
+//    }
+// }
+
 function getNewImage() {
-   try {
+   
       fetch(unsplash)
          .then((response) => {
             responseURL = response.url;
             imgCtn.innerHTML = `<img src="${responseURL}">`;
          })
-   } catch (e) {
-      console.log(e);
-      warningFailAPI.style.display = "block";
-      setTimeout(warningfailAPIHide, 5000);
-   }
+         .catch((response) => {
+            warningFailAPI.style.display = "block";
+            setTimeout(warningfailAPIHide, 5000);
+            response;
+         })
+   
 }
+
+
+
+// function getNewImagePromise() {
+// return new Promise((resolve, reject) => { 
+//    fetch(unsplash);
+
+//    if(response.url != ""){
+//       responseURL = response.url;
+//       resolve(imgCtn.innerHTML = `<img src="${responseURL}">`);
+//    } else{
+//       reject({warningFailAPI.style.display = "block"
+//       setTimeout(warningfailAPIHide, 5000)}
+//    }
+// }
+
+
 
 function disableButton() {
    submit.disabled = true;
@@ -111,8 +95,8 @@ submit.addEventListener("click", () => {
       let linkNew = document.createElement("a");
       linkNew.innerHTML = `<img src="${responseURL}" <br> <p>${emailValue}</p>`;
       rightCtn.appendChild(linkNew);
-      getNewImage();
-      disableButton();
+      // getNewImage();
+      // disableButton();
    } else {
       warning.style.display = "block";
       setTimeout(warningHide, 5000)
